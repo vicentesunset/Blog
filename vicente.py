e59aaa8bb8178c71c1b7d4c59448b0771af6c2ba
vicentesunset <153086892+vicentesunset@users.noreply.github.com>
2023-12-07 08:03:50 +0800

    Add files via upload

---
+++ vicente.py
@@ -0,0 +1,13 @@
+import streamlit as st
+
+
+st.set_page_config(page_title="Vicente's Weblog", page_icon=":tada:", layout="wide")
+
+with st.container():
+    st.title("Kunnichiwa! Vicente's Weblog.")
+    st.write("I'm Vicente Bautista Jr. or you can call me Centeyy. I'm 18 years old and I live in Purok-1 Marga, Tubod Surigao del Norte. My favorite color is Red. My favorite shape is Triangle. My favorite pet is Cat, Dog, and Rabbit. My favorite number is 4. My hobbies are Reading Wattpad, reading Manga, Watching Anime, Singing and dancing.")
+    st.image("vicente.jpg", caption="A K-pop fun and a Computer Engineering Student in SNSU")
+    st.write("College is certainly an interesting time in life. It is filled with new experiences, new people, and new opportunities. As a college student, I faced with many challenges, ranging from managing my studies to living on my own for the first time. It can certainly be an overwhelming time at first, but can also be a period of great growth. As I progress through college, I undoubtedly make new friends, explore ideas, and expand my knowledge. I even discover different interests that will help shape my future path in life. So, I embrace my college years and make the most out of every experience.")
+    st.write("This is my facebook account")
+    st.markdown("https://www.facebook.com/vicentejrxx")
+    
